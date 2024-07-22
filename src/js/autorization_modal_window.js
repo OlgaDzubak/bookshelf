338fc6a2554a6a4f61fb79797859ac6a313e2864
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-07-22 04:08:08 +0300

    update

--- src/js/autorization_modal_window.js
+++ src/js/autorization_modal_window.js
@@ -1,43 +1,80 @@
 import Notiflix from 'notiflix';
 
-const openBtn = document.querySelector('.jsOpenBtn');
-const openBtnMobile = document.querySelector('.jsOpenBtn-mobile');
-
 const backdrop = document.querySelector('.autorization-modal-backdrop');
-const closeBtn = document.querySelector('.close-button');
-const modalForm = document.querySelector('.modalForm');
 
-openBtn.addEventListener('click', onOpenModal);
-openBtnMobile.addEventListener('click', onOpenModal);
-closeBtn.addEventListener('click', onCloseModal);
-
-function onOpenModal() {
-  window.addEventListener('keydown', onEscKeyDown);
-  backdrop.classList.remove('is-hidden');
-  document.body.classList.add('block-scroll');
-}
-function onCloseModal() {
-  window.removeEventListener('keydown', onEscKeyDown);
-  backdrop.classList.add('is-hidden');
-  document.body.classList.remove('block-scroll');
-}
-
-modalForm.addEventListener('submit', onFormSubmit);
-
-function onFormSubmit(e) {
-  e.preventDefault();
-  const inputName = modalForm.elements.name.value;
-  const inputEmail = modalForm.elements.email.value;
-  const inputPassword = modalForm.elements.password.value;
-
-  if (inputName === '' || inputEmail === '' || inputPassword === '') {
-    return Notiflix.Notify.failure('Please fill in all fields!');
+//відкриття модального вікна для авторизації
+  const openBtn = document.querySelector('.jsOpenBtn');
+  const openBtnMobile = document.querySelector('.jsOpenBtn-mobile');
+  openBtn.addEventListener('click', onOpenModal);
+  openBtnMobile.addEventListener('click', onOpenModal);
+  function onOpenModal() {
+    window.addEventListener('keydown', onEscKeyDown);
+    backdrop.classList.remove('is-hidden');
+    document.body.classList.add('block-scroll');
   }
-  modalForm.reset();
-}
 
-function onEscKeyDown(e) {
-  if (e.code === 'Escape') {
-    onCloseModal();
+
+//обробка кнопки закриття модального вікна
+  const closeBtn = document.querySelector('.close-button');
+  closeBtn.addEventListener('click', onCloseModal);
+  function onCloseModal() {
+    window.removeEventListener('keydown', onEscKeyDown);
+    backdrop.classList.add('is-hidden');
+    document.body.classList.remove('block-scroll');
   }
-}
+  function onEscKeyDown(e) {
+    if (e.code === 'Escape') {
+      onCloseModal();
+    }
+  }
+
+//Обробка кнопки сабміта форми
+  const modalForm = document.querySelector('.modalForm');
+  const submitBtn = document.querySelector('.submitBtn');
+  modalForm.addEventListener('submit', onFormSubmit);
+  function onFormSubmit(e) {
+    e.preventDefault();
+    if (submitBtn.textContent === "SIGN UP"){
+      
+      const inputName = modalForm.elements.name.value;
+      const inputEmail = modalForm.elements.email.value;
+      const inputPassword = modalForm.elements.password.value;
+    
+      if (inputName === '' || inputEmail === '' || inputPassword === '') {
+        return Notiflix.Notify.failure('Please fill in all fields!');
+      }
+
+
+    }else{
+      const inputEmail = modalForm.elements.email.value;
+      const inputPassword = modalForm.elements.password.value;
+    
+      if (inputEmail === '' || inputPassword === '') {
+        return Notiflix.Notify.failure('Please fill in all fields!');
+      }
+    }
+
+
+    modalForm.reset();
+  }
+
+//Обробка кнопок sign-up та sign-in під сабмітом
+  const signUpBtn = document.querySelector('.btn-sign-up');
+  const signInBtn = document.querySelector('.btn-sign-in');
+  const name = document.querySelector('.li-name');
+  signUpBtn.addEventListener('click', onsignUpBtnClick);
+  signInBtn.addEventListener('click', onsignInBtnClick);
+  function onsignUpBtnClick(){
+    name.classList.remove("is-hidden");
+    submitBtn.textContent = "SIGN UP"
+    signUpBtn.classList.add("current");
+    signInBtn.classList.remove("current");  
+  }
+  function onsignInBtnClick(){
+    name.classList.add("is-hidden");
+    submitBtn.textContent = "SIGN IN"
+    signUpBtn.classList.remove("current");
+    signInBtn.classList.add("current");
+  }
+
+
