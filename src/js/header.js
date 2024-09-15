29f1b3517087e9914f0355b398f402abedb4038e
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-09-16 00:04:36 +0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -27,6 +27,11 @@ async function showHeader(){
             abortCtrl = new AbortController();
             const {user} = await api.refreshUser(accessToken, abortCtrl);     // отримуэмо дані про юзера з сервера
             if (user){
+                if (user.accessToken != JSON.parse(localStorage.getItem("bookshelfAccessToken"))){
+                    let date = new Date(Date.now() + (3 * 60 * 1000));
+                    date = date.toUTCString();          
+                    document.cookie = `bookshelfAccessToken=${data.accessToken}; expires=${date},  ;secure;`;
+                }
                 headerAuthorised(user);                
             }else{
                 throw new Error("Not authorized");
