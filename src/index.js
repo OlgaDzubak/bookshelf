9d6b575e92871a89c9fda204651569f9d4b990f9
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-09-20 11:00:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './js/header';
-//import './js/isChangeTheme';
-//import './js/support_ukraine';
-//import './js/books';
+import './js/isChangeTheme';
+import './js/support_ukraine';
+import './js/books';
 
-// import './js/autorization_modal_window';
-// import './js/book_modal_window';
-// import './js/mobile_menu';
-// import './js/about_us_modal_window';
+import './js/autorization_modal_window';
+import './js/book_modal_window';
+import './js/mobile_menu';
+import './js/about_us_modal_window';
 
 
-// const homeLinkEl = document.querySelector('.js-nav-homelink');
-// const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
-// homeLinkEl.classList.toggle('selected');
-// mobileHomeLinkEl.classList.toggle('mobile-selected');
+const homeLinkEl = document.querySelector('.js-nav-homelink');
+const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
+homeLinkEl.classList.toggle('selected');
+mobileHomeLinkEl.classList.toggle('mobile-selected');
