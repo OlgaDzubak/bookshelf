7c847c4bed45aa7437737cf00d24c4e3a19ef361
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-10-02 20:10:03 +0300

    Update API.js

--- src/js/API.js
+++ src/js/API.js
@@ -90,7 +90,7 @@ export class bookshelf_API {
     async addToShoppingList(accessToken, bookId ,abortCtrl){
       console.log("accessToken=", accessToken);
       this.setAuthHeader(accessToken);
-      const response = await axios.post(`${this.#BASE_URL}books/shoppinglist/add/:${bookId}`, {signal: abortCtrl.signal});
+      const response = await axios.post(`${this.#BASE_URL}books/shoppinglist/add/${bookId}`, {signal: abortCtrl.signal});
 
       return response;
     }
