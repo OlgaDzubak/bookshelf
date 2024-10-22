ca3b1e0a2329e85b274ea6f136d1c75f9834cfb8
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-10-22 23:37:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './js/support_ukraine';
 import './js/books';
 
 import './js/autorization_modal_window';
+import './js/user_profile_modal_window';
 import './js/book_modal_window';
 import './js/mobile_menu';
 import './js/about_us_modal_window';
