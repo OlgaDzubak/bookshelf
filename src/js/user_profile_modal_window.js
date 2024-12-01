502d9a3cecd304f254ae5126ade7e7f28a5953f9
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-01 21:09:31 +0200

    Update user_profile_modal_window.js

--- src/js/user_profile_modal_window.js
+++ src/js/user_profile_modal_window.js
@@ -14,7 +14,7 @@ export function onAnyKeyDownProfileModal({target, code}){
    
     console.log(target.classList.contains('user-profile-input'));
     
-    if (!target.classList.contains('user-profile-input')){
+    if (!target.classList.contains('user-profile-input') || code === 'Escape') {
         onCloseProfileModal();
     }    
 }
