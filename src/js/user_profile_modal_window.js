506c41cb7d3027516e869618bffef4dcd995ea36
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-01 20:11:53 +0200

    Update user_profile_modal_window.js

--- src/js/user_profile_modal_window.js
+++ src/js/user_profile_modal_window.js
@@ -1,11 +1,13 @@
 const userProfileCloseBtn = document.querySelector(".user-profile-closeBtn");
 const userProfileModal =  document.querySelector(".user-profile-modal");
 
+if (!userProfileModal.classList.contains("is-hidden")){
+    window.addEventListener('keydown', onAnyKeyDown);
+    window.addEventListener('mousemove', onAnyKeyDown);
+    window.addEventListener('touchstart', onAnyKeyDown);
 
-window.addEventListener('keydown', onAnyKeyDown);
-window.addEventListener('mousemove', onAnyKeyDown);
-window.addEventListener('touchstart', onAnyKeyDown);
-userProfileCloseBtn.addEventListener("click", onCloseProfileModal);
+    userProfileCloseBtn.addEventListener("click", onCloseProfileModal);
+}
 
 
 function onCloseProfileModal(){
