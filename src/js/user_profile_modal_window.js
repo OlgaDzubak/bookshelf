34d59074e37868a90306db2d41de7e6d430425a3
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-02 17:17:13 +0200

    Update user_profile_modal_window.js

--- src/js/user_profile_modal_window.js
+++ src/js/user_profile_modal_window.js
@@ -1,14 +1,15 @@
-const userProfileCloseBtn = document.querySelector(".user-profile-closeBtn");
 const userProfileModal =  document.querySelector(".user-profile-modal");
+const userProfileCloseBtn = document.querySelector(".user-profile-closeBtn");
+const userProfileAddPhotoBtn =  document.querySelector(".add-photo-btn");
 
-userProfileCloseBtn.addEventListener("click", onCloseProfileModal);
+userProfileCloseBtn.addEventListener("click", onCloseProfileModalClick);
+userProfileAddPhotoBtn.addEventListener("click", onAddPhotoBtnClick);
 
-
-export function onCloseProfileModal(){
+export function onCloseProfileModalClick(){
     
     window.removeEventListener('keydown', onAnyKeyDownProfileModal);
     window.removeEventListener('mousedown', onAnyKeyDownProfileModal );
-    window.removeEventListener('scroll', onCloseProfileModal );
+    window.removeEventListener('scroll', onCloseProfileModalClick );
   
     userProfileModal.classList.add("is-hidden");
 }
@@ -17,6 +18,11 @@ export function onCloseProfileModal(){
 export function onAnyKeyDownProfileModal({target, code}){
   
     if (!target.classList.contains('user-profile-input') || code === 'Escape') {
-        onCloseProfileModal();
+        onCloseProfileModalClick();
     }    
+}
+
+function onAddPhotoBtnClick(){
+  // дописати обрання файла фото
+  
 }
