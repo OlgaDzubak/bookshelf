5298000d95a48e8ff5b185b2448fe00dea977857
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-05 18:16:54 +0200

    Update API.js

--- src/js/API.js
+++ src/js/API.js
@@ -64,11 +64,16 @@ export class bookshelf_API {
         this.setAuthHeader(accessToken);
 
         axios.defaults.withCredentials = true;
-        axios.defaults.headers.patch['Content-Type'] = 'multipart/form-data';
+     //   axios.defaults.headers.patch['Content-Type'] = 'multipart/form-data';
 
         console.log("name=", formData.get("name"));
 
-        const {data} = await axios.patch(`${this.#BASE_URL}users/update`, formData, {signal: abortCtrl.signal});
+        const {data} = await axios.patch(`${this.#BASE_URL}users/update`, 
+                                                                formData, 
+                                                                { headers: {'Content-Type': 'multipart/form-data',},
+                                                                  signal: abortCtrl.signal
+                                                                }
+                                        );
 
         return data;   
 
