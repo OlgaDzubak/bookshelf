de9ce43fc227607e2672caeb0b5733bfe7c58846
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-16 00:07:18 +0200

    Update user_profile_modal_window.js

--- src/js/user_profile_modal_window.js
+++ src/js/user_profile_modal_window.js
@@ -40,6 +40,7 @@ function onChangePhotoFile({target}){
    var file = target.files[0];
    const maxSizeFile = 5 * 1024 * 1024;
 
+   console.log(file.type.slice(5));
     if (file.type.slice(5) != "image"){
         Notify.failure('Wrong file format. Please choose image file.', {
             position: 'right-center',
