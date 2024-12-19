415d663cd34e9364f92be21d49091d9fa62b3647
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-19 15:07:44 +0200

    Update user_profile_modal_window.js

--- src/js/user_profile_modal_window.js
+++ src/js/user_profile_modal_window.js
@@ -110,18 +110,21 @@ async function onUserProfileModalFormSubmit(e){
             console.log(error);
           
             if (error === "Not authorized") {
-
                 document.cookie = 'accessToken=;  max-age=-1;';
                 userProfileModal.classList.add("is-hidden");
                 userProfileInput.value = "";
                 headerNotAuthorised();
-              
-            } else if(error === 'Wrong file format!") {
-                      
+            }else if (error === 'Wrong file format!"){
                 Notify.failure('Wrong file format! Only png/jpg/jpeg file are allowed.', {
                        position: 'right-center',
                        distance: '100px',
-                })
-          
-            }   
+                });
+            }else{
+                Notify.failure('Profile uploading failed. Please reload the page and try again.', {
+                       position: 'right-center',
+                       distance: '100px',
+                });
+            }
+          }
+  
 }
