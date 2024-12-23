f5150283f4877a784e29268a558f5dc6b327dba1
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-23 17:21:10 +0200

    update

---
+++ src/js/logout_modal_window.js
@@ -0,0 +1,54 @@
+import { bookshelf_API } from './API';
+import {createLoader, headerNotAuthorised} from './help_functions';
+
+const api = new bookshelf_API();
+
+const logoutModal =  document.querySelector(".logout-modal");
+const logoutCloseBtn = document.querySelector(".logout-closeBtn");
+const logoutForm =  document.querySelector(".logout-form");
+
+logoutCloseBtn.addEventListener("click", onCloseLogoutModal);
+logoutForm.addEventListener("submit", onLogoutFormSubmit);
+
+
+let abortCtrl1, loader;
+
+function onCloseLogoutModal(){
+
+        window.removeEventListener('keydown', onAnyKeyDownLogoutModal);
+        window.removeEventListener('mousedown', onAnyKeyDownLogoutModal);
+        window.removeEventListener('scroll', onCloseLogoutModal);
+      
+        logoutModal.classList.add("is-hidden");
+}
+
+export function onAnyKeyDownLogoutModal({target, code}){
+   
+    if (!target.classList.contains('logout-elm') || code === 'Escape') {
+        onCloseLogoutModal();
+    }    
+}
+
+async function onLogoutFormSubmit(){
+    e.preventDefault();
+
+    if (abortCtrl1) {
+        abortCtrl1.abort();
+    }
+
+    try{
+
+        const accessToken = getCookie("accessToken");        
+ 
+        loader = createLoader(userProfileModal, "into");
+        loader.classList.add("loader-modal");
+        loader.classList.add("logout-elm");
+        
+        abortCtrl1 = new AbortController();
+        const data = await api.logout({accessToken}, abortCtrl1);
+        
+    }catch(error){
+        loader.remove();
+        console.dir(error.message);
+    }
+}
