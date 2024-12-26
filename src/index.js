79804f5b56d77089546021a24a0455d1215aea73
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-26 17:57:33 +0200

    update

--- src/index.js
+++ src/index.js
@@ -10,8 +10,21 @@ import './js/book_modal_window';
 import './js/mobile_menu';
 import './js/about_us_modal_window';
 
+import {createMobileHeaderMarkUp, createNonMobileHeaderMarkUp} from './js/header'
+import {createMobileMenuMarkUp} from './js/mobile_menu';
 
-const homeLinkEl = document.querySelector('.js-nav-homelink');
-const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
-homeLinkEl.classList.toggle('selected');
-mobileHomeLinkEl.classList.toggle('mobile-selected');
+
+const pageWidth = document.documentElement.scrollWidth ; 
+
+if (pageWidth < 768) { 
+    createMobileHeaderMarkUp();
+    createMobileMenuMarkUp();
+}else{
+    createNonMobileHeaderMarkUp();
+}
+
+
+// const homeLinkEl = document.querySelector('.js-nav-homelink');
+// const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
+// homeLinkEl.classList.toggle('selected');
+// mobileHomeLinkEl.classList.toggle('mobile-selected');
