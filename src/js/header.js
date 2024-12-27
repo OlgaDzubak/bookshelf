720cf515c3f691963bc9f591cf43ef6f843c3006
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-27 22:03:03 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -63,30 +63,37 @@ function headerNotAuthorised(){
 }
 function headerAuthorised(user){
 
+    const navigation = document.querySelector('.navigation');
     const userLoginBtn = document.querySelector('.user-login-btn');
     const authBtn = document.querySelector('.auth-btn');
-    const navigation = document.querySelector('.navigation');
     const authBtnImg= authBtn.querySelector('.user-img');
     const authBtnName= authBtn.querySelector('.login-p');
+    const authMobileBtn = document.querySelector('.mobile-menu .auth-btn');
+    const authMobileBtnImg= authBtn.querySelector('.mobile-menu .user-img');
+    const authMobileBtnName= authBtn.querySelector('.mobile-menu .login-p');
     const userPhotoImg = document.querySelector('.user-photo-img');
     const userMobileMenuLogoutBtn = document.querySelector('.mobile-menu .logout-btn');
 
     authBtnName.textContent = user.name;
-   
+    authMobileBtnName.textContent = user.name;
 
     if (user.avatarURL){
         authBtnImg.src = user.avatarURL;
+        authMobileBtnImg.src = user.avatarURL;
         userPhotoImg.src = user.avatarURL;
     }else{
         authBtnImg.src = userIcon;
+        authMobileBtnImg.src = userIcon;
         userPhotoImg.src = userIcon;
     }
     
     userLoginBtn.classList.add("is-hidden");
+        
     authBtn.classList.remove("is-hidden");
-    navigation.classList.remove("is-hidden");
+    authMobileBtn.classList.remove("is-hidden");
     userMobileMenuLogoutBtn.classList.remove("is-hidden");
-
+    navigation.classList.remove("is-hidden");    
+    
     if (user.shopping_list.length > 0){
         displayOrdredAmountInShoppingBag(user.shopping_list);
         localStorage.setItem("bookshelf_orderedbooks",JSON.stringify(user.shopping_list));
