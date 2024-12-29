c43955e284c82824a711d618d3f6b63e384da896
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-29 20:24:36 +0200

    update

--- src/index.js
+++ src/index.js
@@ -11,6 +11,5 @@ import './js/modal_windows/book_modal_window';
 import './js/modal_windows/about_us_modal_window';
 
 
-//document.querySelector('.mobile-menu').classList.add('is-hidden');
 document.querySelector('.header .home-link').classList.toggle('selected');
 document.querySelector('.mobile-menu .home-link').classList.toggle('mobile-selected');
