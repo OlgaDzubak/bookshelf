ed1d71bdc9613e63630ee0d2c823c2af6af8def0
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2025-01-01 21:31:58 +0200

    Update book_modal_window.js

--- src/js/modal_windows/book_modal_window.js
+++ src/js/modal_windows/book_modal_window.js
@@ -70,7 +70,8 @@ async function createModalWindow(book_Id) {
         const authorEl = document.querySelector('#author');
         const descriptionEl = document.querySelector('#description');
         const marketPlEl = document.querySelector('.market_placers_list');
-     
+        const authBtn = document.querySelector('.auth-btn');
+
         nameBookEl.textContent = title;
         authorEl.textContent = author;
 
@@ -131,12 +132,20 @@ async function createModalWindow(book_Id) {
 
         divBackdropEl.classList.toggle('is-hidden');
         const orderedBookID_arr = JSON.parse(localStorage.getItem('bookshelf_orderedbooks'));
+        
+        if (authBtn.classList.includes("is-hidden") >= 0){
 
-        if (orderedBookID_arr === null || !orderedBookID_arr.includes(book_Id)) {
-            btnAddEl.classList.remove('is-hidden');
-        } else {
-            btnRemoveEl.classList.remove('is-hidden');
-            textEl.classList.remove('is-hidden');
+            btnAddEl.setAttribute("disabled");
+            textEl.textContent = 'Please sign up/sign in to be able to add/remove books to shopping list!';
+            
+        }else{
+
+            if (orderedBookID_arr === null || !orderedBookID_arr.includes(book_Id)) {
+                btnAddEl.classList.remove('is-hidden');
+            } else {
+                btnRemoveEl.classList.remove('is-hidden');
+                textEl.classList.remove('is-hidden');
+            }
         }
 
     } catch (error) {
