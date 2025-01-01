cdf2bd8c744a08a17107a3dcbb053d1dcd4985d6
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2025-01-01 23:13:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ document.querySelector('.header .home-link').classList.toggle('selected');
 document.querySelector('.mobile-menu .home-link').classList.toggle('mobile-selected');
 
 window.addEventListener('popstate', function(event){
+    console.log(event);
     if (event.state && event.state.id){
         console.log(`Перехід до сторінки з ID: ${event.state.id}`);
     }
