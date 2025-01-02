ea95cc6d95d8b1f64baab53cdc44b0af9e00f891
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2025-01-02 02:56:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,18 +14,8 @@ import './js/modal_windows/about_us_modal_window';
 document.querySelector('.header .home-link').classList.toggle('selected');
 document.querySelector('.mobile-menu .home-link').classList.toggle('selected');
 
-// window.addEventListener('popstate', function(event){
-//     console.log("popstate event= ", event);
-//     if (event.state && event.state.id){
-//         console.log(`Перехід до сторінки з ID: ${event.state.id}`);
-//     }
-// });
-
 window.addEventListener('pageshow', (event) => {
     if (event.persisted) {
-      console.log('This page was restored from the bfcache.');
       location.reload();
-    } else {
-      console.log('This page was loaded normally.');
     }
   });
